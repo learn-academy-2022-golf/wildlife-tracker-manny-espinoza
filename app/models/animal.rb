d1d3d46ef935b953a61e9a1a381f6782486deb92
manny-espinoza <mannyespinoza123@yahoo.com>
2022-12-14 10:16:54 -0800

    Completed Story 4, awaiting review and approval

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,6 @@
 class Animal < ApplicationRecord
     has_many :sightings
+    validates :common_name, :scientific_binomial, presence: true
+    validates :common_name, :scientific_binomial, uniqueness: true
+    validates :common_name, comparison: { other_than: :scientific_binomial }
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is not valid without a common name' do
+    fish = Animal.create scientific_binomial:"Galeocerdo Cuvier"
+    expect(fish.errors[:common_name]).to_not be_empty
+  end
+  it 'is not valid without a scientific binomial' do
+    fish = Animal.create common_name:"Tiger Shark"
+    expect(fish.errors[:scientific_binomial]).to_not be_empty
+  end
+  it 'is not valid if common name and scientific binomial are the same' do
+    fish = Animal.create common_name:"Tiger Shark", scientific_binomial:"Tiger Shark"
+    expect(fish.errors[:common_name]).to_not be_empty
+  end
+  it 'is not valid if common name is not unique' do
+    Animal.create common_name:"Tiger Shark", scientific_binomial:"Galeocerdo Cuvier"
+    fish = Animal.create common_name:"Tiger Shark", scientific_binomial:"Galeocerdo Cuvier"
+    expect(fish.errors[:common_name]).to_not be_empty
+  end
+  it 'is not valid if scientific binomial is not unique' do
+    Animal.create common_name:"Tiger Shark", scientific_binomial:"Galeocerdo Cuvier"
+    fish = Animal.create common_name:"Tiger Shark", scientific_binomial:"Galeocerdo Cuvier"
+    expect(fish.errors[:scientific_binomial]).to_not be_empty
+  end
 end
